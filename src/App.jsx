22c3d0f88905af0ae8654ab8c9e1b5a91e201294
egwuatu modestus <egwuatumodestus@gmail.com>
2025-05-07 01:36:20 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-export default function App() {
+import React from 'react'
+import Navbar from './Components/Navbar';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+
+
+
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Here We Go, Here We Go, Ohoo!
-    </h1>
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>}/>
+
+
+      </Routes>
+
+
+
+    </div>
   )
 }
+
+export default App
+
